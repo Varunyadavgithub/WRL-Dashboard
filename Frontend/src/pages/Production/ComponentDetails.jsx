41b54d8f6a5c3ce4315f159820dadd5beb0facef
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-05-28 10:43:23 +0530

    Component Details page done

--- Frontend/src/pages/Production/ComponentDetails.jsx
+++ Frontend/src/pages/Production/ComponentDetails.jsx
@@ -1,7 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Title from "../../components/common/Title";
+import InputField from "../../components/common/InputField";
+import Button from "../../components/common/Button";
+import ExportButton from "../../components/common/ExportButton";
+
 const ComponentDetails = () => {
+  const [loading, setLoading] = useState(false);
+  const [serialNumber, setSerialNumber] = useState("");
+  const [componentDetailsData, setComponentDetailsData] = useState([]);
+
+  const fetchComponentDetailsData = async () => {
+    if (!serialNumber) {
+      toast.error("Please select Serial Number.");
+      return;
+    }
+    try {
+      setLoading(true);
+      const res = await axios.get(`${baseURL}prod/component-details`, {
+        params: { serialNumber },
+      });
+      const data = res.data?.result?.recordsets[0] || [];
+      setComponentDetailsData(data);
+    } catch (error) {
+      console.error("Failed to fetch production data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <div>
-      <h1>Component Details</h1>
+    <div className="p-6 bg-gray-100 min-h-screen rounded-lg">
+      <Title title="Component Details" align="center" />
+
+      {/* Filters Section */}
+      <div className="bg-purple-100 border border-dashed border-purple-400 p-4 mt-4 rounded-md">
+        {/* First Row */}
+        <div className="flex flex-wrap items-center gap-4">
+          <InputField
+            label="Serial Number"
+            type="text"
+            placeholder="Enter details"
+            className="w-64"
+            name="serialNumber"
+            value={serialNumber}
+            onChange={(e) => setSerialNumber(e.target.value)}
+          />
+          <div className="flex items-center justify-center gap-4">
+            <Button
+              bgColor={loading ? "bg-gray-400" : "bg-blue-500"}
+              textColor={loading ? "text-white" : "text-black"}
+              className={`font-semibold ${loading ? "cursor-not-allowed" : ""}`}
+              onClick={() => fetchComponentDetailsData()}
+              disabled={loading}
+            >
+              Query
+            </Button>
+            <ExportButton
+              data={componentDetailsData}
+              filename="stage_history_report"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Summary Section */}
+      <div className="bg-purple-100 border border-dashed border-purple-400 p-4 mt-4 rounded-md">
+        <div className="bg-white border border-gray-300 rounded-md p-4">
+          <h1>Table</h1>
+        </div>
+      </div>
     </div>
   );
 };
