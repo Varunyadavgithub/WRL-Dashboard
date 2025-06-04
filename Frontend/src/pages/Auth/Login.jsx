93c70d25868871b5dbe0b28f032a40963ec7bd38
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-06-04 09:58:34 +0530

    added show hide icon in password field

--- Frontend/src/pages/Auth/Login.jsx
+++ Frontend/src/pages/Auth/Login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Title from "../../components/common/Title";
-import InputField from "../../components/common/InputField";
 import Button from "../../components/common/Button";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -8,6 +7,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setAuthUser } from "../../redux/authSlice.js";
 import { assets } from "../../assets/assets.js";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
@@ -18,6 +18,8 @@ const Login = () => {
     empcod: "",
     password: "",
   });
+  const [showPass, setShowPass] = useState(false);
+
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -77,25 +79,42 @@ const Login = () => {
           />
 
           <form onSubmit={handleSubmit} className="mt-2">
-            <InputField
-              label="Employee Code"
-              type="text"
-              placeholder="Enter your employee code"
-              value={formData.empcod}
-              onChange={handleChange}
-              name="empcod"
-              required
-            />
-
-            <InputField
-              label="Password"
-              type="password"
-              placeholder="Enter your password"
-              value={formData.password}
-              onChange={handleChange}
-              name="password"
-              required
-            />
+            <div className="mb-4">
+              <label className="block text-md font-semibold text-gray-700 mb-1">
+                Employee Code
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your employee code"
+                value={formData.empcod}
+                onChange={handleChange}
+                name="empcod"
+                className="w-full px-2 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-md font-semibold text-gray-700 mb-1">
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  type={showPass ? "text" : "password"}
+                  placeholder="Enter your password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  name="password"
+                  className="w-full px-2 py-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+                <span
+                  onClick={() => setShowPass(!showPass)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600 hover:text-gray-800 cursor-pointer"
+                >
+                  {showPass ? <FaEyeSlash size={18} /> : <FaEye size={18} />}
+                </span>
+              </div>
+            </div>
 
             <Button type="submit" className="w-full mt-6" disabled={loading}>
               {loading ? "Logging in..." : "Login"}
