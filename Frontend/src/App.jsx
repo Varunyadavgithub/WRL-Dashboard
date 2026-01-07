db1d7dd003493bf9056e428cfe9f846798bcd68b
buildwithvikash <vk117037@gmail.com>
2026-01-07 17:58:16 +0530

    feat: Add Training Management Features
    - Implement NominationTab component for employee nominations.
    - Create OverviewTab component to display training details.
    - Develop TrainingCard component for individual training representation.
    - Build TrainingDashboard page to manage and view training sessions.
    - Add TrainingDetails page for detailed training information and tabs for overview, nomination, materials, and attendance.
    - Introduce TrainingList page to list all trainings with status badges.
    - Create utility functions for training duration and date formatting.
    - Establish training permissions and access utilities for role-based access control.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { lazy, Suspense, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
+import TrainingDashboard from "./pages/Training/TrainingDashboard";
 
 // Lazy loaded components
 const Layout = lazy(() => import("./components/Layout"));
@@ -87,6 +88,11 @@ const LogisticsDisplay = lazy(() =>
 );
 
 const Calibiration = lazy(() => import("./pages/Compliance/Calibration"));
+
+const TraningDashboard = lazy(() =>
+  import("./pages/Training/TrainingDashboard")
+);
+const TraningDetail = lazy(() => import("./pages/Training/TrainingDetails"));
 
 const NotFound = lazy(() => import("./pages/NotFound"));
 
@@ -311,6 +317,19 @@ function App() {
               <Route
                 path="/compliance/calibiration"
                 element={<Calibiration />}
+              />
+            )}
+            {/*-------------------------------------------------------------- Traning --------------------------------------------------------------*/}
+            {canAccess(["admin"]) && (
+              <Route
+                path="/training/training-dashboard"
+                element={<TraningDashboard />}
+              />
+            )}
+            {canAccess(["admin"]) && (
+              <Route
+                path="/training/training-detail"
+                element={<TraningDetail />}
               />
             )}
             {/*-------------------------------------------------------------- Catch All --------------------------------------------------------------*/}
